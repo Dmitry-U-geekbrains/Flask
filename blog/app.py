51801ceae7cf85ee5c35d7cb1268909a2e87d9c8
User <ushakov.ip@yandex.ru>
2023-03-18 16:25:56 +0300

    task_4

--- blog/app.py
+++ blog/app.py
@@ -7,23 +7,28 @@ from time import time
 from werkzeug.exceptions import BadRequest
 from blog.models.database import db
 from blog.views.auth import login_manager, auth_app
+import os
+from flask_migrate import Migrate
 
 
 
-app = Flask(__name__)
+migrate = Migrate(app, db)
 
+def create_app() -> Flask:
+    app = Flask(__name__)
+    app.register_blueprint(users_app, url_prefix="/users")
+    app.register_blueprint(articles_app, url_prefix="/articles")
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/blog.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
+    app.config["SECRET_KEY"] = "abcdefg123456"
+    app.register_blueprint(auth_app, url_prefix="/auth")
+    login_manager.init_app(app)
 
-app.register_blueprint(users_app, url_prefix="/users")
-app.register_blueprint(articles_app, url_prefix="/articles")
+    cfg_name = os.environ.get("CONFIG_NAME") or "ProductionConfig"
+    app.config.from_object(f"blog.configs.{cfg_name}")
+    return app
 
-
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/blog.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db.init_app(app)
-
-app.config["SECRET_KEY"] = "abcdefg123456"
-app.register_blueprint(auth_app, url_prefix="/auth")
-login_manager.init_app(app)
 
 
 
