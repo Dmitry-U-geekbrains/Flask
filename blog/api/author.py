56181c500b0f69b92614b5b313068a67ea24db7c
User <ushakov.ip@yandex.ru>
2023-03-26 20:39:38 +0300

    tasksk_10

--- blog/api/author.py
+++ blog/api/author.py
@@ -2,7 +2,10 @@ from flask_combo_jsonapi import ResourceDetail, ResourceList
 
 from blog.schemas import AuthorSchema
 from blog.models.database import db
-from blog.models import Author
+
+from combojsonapi.event.resource import EventsResource
+from blog.models import Author, Article
+
 
 
 class AuthorList(ResourceList):
@@ -12,8 +15,13 @@ class AuthorList(ResourceList):
     "model": Author,
     }
 
+class AuthorDetailEvents(EventsResource):
+    def event_get_articles_count(self, **kwargs):
+        return {"count": Article.query.filter(Article.author_id == kwargs["id"]).count()}
+
 
 class AuthorDetail(ResourceDetail):
+    events = AuthorDetailEvents
     schema = AuthorSchema
     data_layer = {
     "session": db.session,
