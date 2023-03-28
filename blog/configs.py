286847607bfbc44ec1ce747f118a9adc1b695780
User <ushakov.ip@yandex.ru>
2023-03-28 22:38:24 +0300

    task_11

--- blog/configs.py
+++ blog/configs.py
@@ -24,3 +24,8 @@ FLASK_ADMIN_SWATCH = 'cosmo'
 OPENAPI_URL_PREFIX = '/api/swagger'
 OPENAPI_SWAGGER_UI_PATH = '/'
 OPENAPI_SWAGGER_UI_VERSION = '3.22.0'
+
+
+class ProductionConfig(BaseConfig):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    SECRET_KEY = os.environ.get("SECRET_KEY")
